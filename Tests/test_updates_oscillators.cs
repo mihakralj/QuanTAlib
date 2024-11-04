5b333bd2ec694125aaa3a1645492f12fad61838e
Miha Kralj <miha.kralj@outlook.com>
2024-11-04 14:57:02 -0800

    semver fix

--- Tests/test_updates_oscillators.cs
+++ Tests/test_updates_oscillators.cs
@@ -20,8 +20,8 @@ public class OscillatorsUpdateTests
     private TBar GetRandomBar(bool IsNew)
     {
         double open = GetRandomDouble();
-        double high = open + Math.abs(GetRandomDouble());
-        double low = open - Math.abs(GetRandomDouble());
+        double high = open + Math.Abs(GetRandomDouble());
+        double low = open - Math.Abs(GetRandomDouble());
         double close = low + ((high - low) * GetRandomDouble());
         return new TBar(DateTime.Now, open, high, low, close, 1000, IsNew);
     }
