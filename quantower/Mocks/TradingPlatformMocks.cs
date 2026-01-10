486ae788d306011b0374cb08cd6cb36652a5bb89
Miha Kralj <miha.kralj@outlook.com>
2026-01-10 15:31:44 -0800

    Adds DeepSource integration to CI
    Integrates DeepSource into the CI workflow to enhance code quality analysis.
    
    This commit adds steps to upload Roslyn, Qodana, and test coverage reports to DeepSource, enabling automated code review and analysis within the CI pipeline.
    It also changes the `LinesSeries` property to be `IReadOnlyList` to avoid issues when consumers of the API modify this property.

--- quantower/Mocks/TradingPlatformMocks.cs
+++ quantower/Mocks/TradingPlatformMocks.cs
@@ -426,7 +426,7 @@ public abstract class Indicator
 
     public int Count => HistoricalData.Count;
 
-    public IList<LineSeries> LinesSeries => _lineSeries.ToArray();
+    public IReadOnlyList<LineSeries> LinesSeries => _lineSeries;
 
     protected void AddLineSeries(LineSeries series)
     {
